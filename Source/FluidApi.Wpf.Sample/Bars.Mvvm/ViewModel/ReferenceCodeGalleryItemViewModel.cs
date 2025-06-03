f21ba7a25a33f036276c0240f4ee0d0c72c89f2d
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2025-06-03 23:16:52 +0200

    Extended sample application with CustomGalleryTemplateSelector

---
+++ Source/FluidApi.Wpf.Sample/Bars.Mvvm/ViewModel/ReferenceCodeGalleryItemViewModel.cs
@@ -0,0 +1,12 @@
+using ActiproSoftware.Windows.Controls.Bars.Mvvm;
+
+namespace Bars.Mvvm.FluidGenerator.Sample;
+
+/// <summary>
+/// Implementation of <see cref="BarGalleryItemViewModel{T}"/> for <see cref="ReferenceCode"/>.
+/// </summary>
+/// <param name="referenceCode"></param>
+public class ReferenceCodeGalleryItemViewModel(ReferenceCode referenceCode) : BarGalleryItemViewModel<ReferenceCode>(referenceCode)
+{
+
+}
