afb06536f31d551f9af496a8e19f372e14e63d28
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2025-07-28 10:57:22 +0200

    Added some more unit tests

--- Source/FluentApi.Generator.Wpf.Tests/ConvenienceApi/BarComboBoxShould.cs
+++ Source/FluentApi.Generator.Wpf.Tests/ConvenienceApi/BarComboBoxShould.cs
@@ -1,6 +1,8 @@
 ﻿using ActiproSoftware.Windows.Controls.Bars.Mvvm;
 using ActiproSoftware.Windows.Input;
 using FluentAssertions;
+using NSubstitute;
+using System.Windows.Media.Imaging;
 using Xunit;
 
 namespace Bars.Mvvm.FluentApi.Generator.Extensions.Wpf.Test;
@@ -24,5 +26,72 @@ public class BarComboBoxShould
 
         comboBox.IsUnmatchedTextAllowed.Should().BeTrue();
         comboBox.UnmatchedTextCommand.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void BarComboBox_BooleanProperty_NoParameter()
+    {
+        // verifies that BarComboBoxViewModel has no overloads for WithUnmatchedTextCommand
+        var comboBox = new BarComboBoxViewModel();
+
+        // ACT
+        comboBox.WithIsEditable();
+
+        comboBox.IsEditable.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BarComboBox_BooleanProperty_WithParameter()
+    {
+        // verifies that BarComboBoxViewModel has no overloads for WithIsEditable
+        var comboBox = new BarComboBoxViewModel();
+
+        // ACT
+        comboBox.WithIsEditable(false);
+
+        comboBox.IsEditable.Should().BeFalse();
+    }
+
+    [Fact]
+    public void BarComboBox_ObservableCollection_WithItem()
+    {
+        // verifies that BarComboBoxViewModel has no overloads for WithUnmatchedTextCommand
+        var comboBox = new BarComboBoxViewModel();
+
+        // ACT
+        comboBox.WithAboveMenuItem(new BarButtonViewModel().WithLabel("hello"));
+
+        comboBox.AboveMenuItems.Count.Should().Be(1);
+        comboBox.AboveMenuItems[0].Should().BeOfType<BarButtonViewModel>();
+    }
+
+    [Fact]
+    public void BarComboBox_ObservableCollection_WithItems()
+    {
+        // verifies that BarComboBoxViewModel has no overloads for WithUnmatchedTextCommand
+        var comboBox = new BarComboBoxViewModel();
+
+        // ACT
+        comboBox.WithAboveMenuItems([new BarButtonViewModel().WithLabel("hello")]);
+
+        comboBox.AboveMenuItems.Count.Should().Be(1);
+        comboBox.AboveMenuItems[0].Should().BeOfType<BarButtonViewModel>();
+    }
+
+    [Fact]
+    public void BarComboBox_HasVariantImages()
+    {
+        var imageProviderMock = Substitute.For<IBarImageProvider>();
+        imageProviderMock.GetImageSource(Arg.Any<string>(), Arg.Any<BarImageSize>())
+            .Returns(x => new BitmapImage());
+        // verifies that BarComboBoxViewModel has no overloads for WithUnmatchedTextCommand
+        var comboBox = new BarComboBoxViewModel()
+            .WithImages(imageProviderMock);
+
+        // ACT
+        comboBox.WithAboveMenuItems([new BarButtonViewModel().WithLabel("hello")]);
+
+        comboBox.AboveMenuItems.Count.Should().Be(1);
+        comboBox.AboveMenuItems[0].Should().BeOfType<BarButtonViewModel>();
     }
 }
