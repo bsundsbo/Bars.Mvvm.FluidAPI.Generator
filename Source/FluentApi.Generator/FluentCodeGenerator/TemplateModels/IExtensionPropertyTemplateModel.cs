3de9a434c08c144ee1cd4bbe3fba08b43846338c
Bjørnar Sundsbø <bjornar.sundsbo@enghouse.com>
2025-10-15 22:42:09 +0200

    Marking extension methods for obsolete properties as obsolete

---
+++ Source/FluentApi.Generator/FluentCodeGenerator/TemplateModels/IExtensionPropertyTemplateModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Bars.Mvvm.FluentApi.Generator.Extensions.Models;
+
+/// <summary>
+/// Interface for property template models that have an obsolete attribute.
+/// </summary>
+internal interface IExtensionPropertyTemplateModel : IExtensionTemplateModel
+{
+    /// <summary>
+    /// Indicates whether the property is obsolete and should be marked as such in the generated code.
+    /// </summary>
+    bool IsObsolete { get; }
+
+    /// <summary>
+    /// Get the name of the property.
+    /// </summary>
+    string PropertyName { get; }
+}
--- Source/FluentApi.Generator/FluentCodeGenerator/TemplateModels/PropertyTemplateModel.cs
+++ Source/FluentApi.Generator/FluentCodeGenerator/TemplateModels/PropertyTemplateModel.cs
@@ -1,11 +1,12 @@
 using Microsoft.CodeAnalysis;
+using System.Linq;
 
 namespace Bars.Mvvm.FluentApi.Generator.Extensions.Models;
 
 /// <summary>
 /// Base model for specific property template models. All these properties are used in various templates when generating code.
 /// </summary>
-internal record PropertyTemplateModel : IExtensionTemplateModel
+internal record PropertyTemplateModel : IExtensionPropertyTemplateModel
 {
     public PropertyTemplateModel(INamedTypeSymbol classSymbol, IPropertySymbol propertySymbol)
     {
@@ -14,35 +15,41 @@ internal record PropertyTemplateModel : IExtensionTemplateModel
         ParameterName = char.ToLowerInvariant(this.PropertyName[0]) + this.PropertyName.Substring(1);
         ClassName = classSymbol.Name;
         NamespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+        var obsoleteAttribute = propertySymbol.GetAttributes()
+            .FirstOrDefault(a => a.AttributeClass?.ToDisplayString() == "System.ObsoleteAttribute");
+        IsObsolete = obsoleteAttribute != null;
     }
+
+    /// <inheritdoc />
+    public bool IsObsolete { get; }
 
     /// <summary>
     /// Namespace name of the class that contains the class where the property is declared.
     /// </summary>
-    public object NamespaceName { get; init; }
+    public object NamespaceName { get; }
 
     /// <summary>
     /// Class declaring the property.
     /// </summary>
-    public object ClassName { get; init; }
+    public object ClassName { get; }
 
     /// <summary>
     /// Formatted parameter name for the property, which is <see cref="PropertyName"/> in camel case.
     /// </summary>
-    public string ParameterName { get; init; }
+    public string ParameterName { get; }
 
     /// <summary>
     /// Type of the property.
     /// </summary>
-    public string PropertyType { get; init; }
+    public string PropertyType { get; }
 
     /// <summary>
     /// Name of the property generating extension method for.
     /// </summary>
-    public string PropertyName { get; init; }
+    public string PropertyName { get; }
 
     public override string ToString()
     {
-        return $"{nameof(NamespaceName)}: {NamespaceName}, {nameof(ClassName)}: {ClassName}, {nameof(ParameterName)}: {ParameterName}, {nameof(PropertyType)}: {PropertyType}, {nameof(PropertyName)}: {PropertyName}";
+        return $"{nameof(NamespaceName)}: {NamespaceName}, {nameof(ClassName)}: {ClassName}, {nameof(ParameterName)}: {ParameterName}, {nameof(PropertyType)}: {PropertyType}, {nameof(PropertyName)}: {PropertyName} IsObsolete: {IsObsolete}";
     }
 }
